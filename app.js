3293c4aa95af6e93cb0332eb0dc1a10e76671e52
Alec Maliwanag <amaliwanag@gmail.com>
2016-10-17 14:48:46 -0700

    Finish adding static data and writing helper functions for base calculations.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+  fs = require('fs'),
+  _ = require('underscore'),
+  Promise = require('bluebird'),
+  request = require('request'),
+  bodyParser = require('body-parser');
+  var util = require('./util.js')
+  // env = require('env'),
+  // mysql = require('mysql');
+
+
+var app = express();
+var riotEndpts = {
+  summonerID: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/',
+  gameLog: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/',
+  apiKey: '?api_key=4a99a25b-dd7e-4698-8256-9b3b773fa3ed'
+}
+
+//================================================================================
+  // Middleware
+//================================================================================
+// app.use(express.static( __dirname + '/../client'));
+
+
+//================================================================================
+  // Routing
+//================================================================================
+//data pulls
+// request('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells,stats&api_key=4a99a25b-dd7e-4698-8256-9b3b773fa3ed').pipe(fs.createWriteStream('champdata.json'))
+console.log('Server is listening on port 1337');
+var data = JSON.parse(fs.readFileSync('champdata.json','utf8'));
+// console.log(data.data['Jinx'].stats);
+console.log('armor calculator', util.lvlCalculator(5,'Maokai'));
+console.log('all the stats for jinx', util.baseCalculatorAllLvls('Nautilus'))
+// console.log(data.data['Maokai'].stats);
+
+app.listen(1337);
---
+++ util.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var data = JSON.parse(fs.readFileSync('champdata.json','utf8')); 
+module.exports = {
+  lvlCalculator : function(level, champion) {
+    var targetChampion = data.data[champion].stats;
+    //Statistic= b+g×(n−1)×(0.685+0.0175×n)  b = base g = growth statistic n = champion level
+    console.log(targetChampion['armorperlevel'], "searching for armor");
+    var baseStats = {
+      level: level,
+      currentArmor: targetChampion['armor'] + targetChampion['armorperlevel'] * (level -1) * (.685 + 0.0175*level),
+      currentAttackDamage : targetChampion['attackdamage'] + targetChampion['attackdamageperlevel'] * (level -1) * (.685 + 0.0175*level),
+      currentAttackSpeed : (.625/(1+ targetChampion['attackspeedoffset'])) + (targetChampion['attackspeedperlevel'] * (level -1) * (.685 + 0.0175*level)/100),
+      currentHp : targetChampion['hp'] + targetChampion['hpperlevel'] * (level -1) * (.685 + 0.0175*level),
+      currentHpRegen : targetChampion['hpregen'] + targetChampion['hpregenperlevel'] * (level -1) * (.685 + 0.0175*level),
+      movespeed : targetChampion['movespeed'],
+      currentMana : targetChampion['mp'] + targetChampion['mpperlevel'] * (level -1) * (.685 + 0.0175*level),
+      currentMpRegen : targetChampion['mpregen'] + targetChampion['mpregenperlevel'] * (level -1) * (.685 + 0.0175*level),
+      currentSpellBlock : targetChampion['spellblock'] + targetChampion['spellblockperlevel'] * (level -1) * (.685 + 0.0175*level)
+    }
+    console.log(currentArmor,'currentArmor');
+    return baseStats;
+  },
+  baseCalculatorAllLvls : function(champion) {
+    var storage = [];
+    for (var i = 1; i <19; i++) {
+      storage.push(this.lvlCalculator(i,champion))
+    }
+    return storage;
+  }
+}
